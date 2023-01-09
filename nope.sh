cbd4e65005a3b7a4bfccedadce2c8c592876e645
_alexou_ <92472779+alexou2@users.noreply.github.com>
2023-01-08 23:24:51 -0500

    shellscript updates
    removed option to open html page in a non firefox browser as it was causing issues on my side. To re-add this feature, temofe the hashtag from line 7 in nope.sh
    
    added option to exit the terminal after openinf the file in firefox

--- nope.sh
+++ nope.sh
@@ -1,9 +1,17 @@
-java src/Main.java
+#!/usr/bin/bash
+java src/Main.java $@
 #echo -e "\n\n\n"
 cat firstChap.txt
 chap=`cat firstChap.txt`
-#firefox $chap
-open $chap
+firefox $chap
+#open $chap
 echo -e "\n\n\n"
 echo $chap
 echo -e "\n\n\n"
+echo "press any key to exit (or press any key to keep using the terminal)"
+read quit
+if [ -z "$quit" ] 
+then
+echo "quit" 
+exit
+fi
